09b0b95998c233209d52cf6b768ef802b42984b2
ndeamador <ndeamador@gmail.com>
2020-11-10 15:07:43 +1100

    5.9 done

--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -5,11 +5,23 @@ import Blog from './Blog'
 const BlogList = ({ blogs, updateBlog }) => {
   console.log('in inbloglist');
 
+  const orderedBlogs = blogs.sort((a, b) => {
+    if (a.likes > b.likes) {
+      return -1
+    } else if (a.likes < b.likes) {
+      return 1
+    } else {
+      return 0
+    }
+  })
+
+  console.log(orderedBlogs);
+
   return (
     <div className="bloglist-container">
       {
         // Remember that in react, each item of a list must be provided a unique key.
-        blogs.map((blog, i) =>
+        orderedBlogs.map((blog, i) =>
           // Remember that class is a keyword in JS and JSX, so className has to be used instead of class when working with React.
           <div className="blog-container" key={i}>
             <Blog key={blog.id} blog={blog} updateBlog={updateBlog}/>
