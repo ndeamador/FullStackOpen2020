e9b49c9f0f1a316dabb0e9d024df467f6c0b4e72
ndeamador <ndeamador@gmail.com>
2020-12-16 21:07:16 +1100

    10.3 done

---
+++ part10/rate-repository-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const RepositoryItem = ({ repository }) => {
+  return (
+    <View>
+      <Text>Full name: {repository.fullName}</Text>
+      <Text>Description: {repository.description}</Text>
+      <Text>Language: {repository.language}</Text>
+      <Text>Stars: {repository.stargazersCount}</Text>
+      <Text>Forks: {repository.forksCount}</Text>
+      <Text>Reviews: {repository.reviewCount}</Text>
+      <Text>Rating: {repository.ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
