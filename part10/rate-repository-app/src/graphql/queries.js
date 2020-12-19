bb4cde8755e54d97e4ffa23ad4cbac00037393fd
ndeamador <ndeamador@gmail.com>
2020-12-19 18:51:35 +1100

    10.11 done

--- part10/rate-repository-app/src/graphql/queries.js
+++ part10/rate-repository-app/src/graphql/queries.js
@@ -1,9 +1,25 @@
 import { gql } from 'apollo-boost';
 
 export const GET_REPOSITORIES = gql`
-  query {
-    repositories {
-      ${/* ... */}
+query {
+  repositories
+   {
+    edges {
+      node {
+        createdAt,
+        description,
+        forksCount,
+        fullName,
+        id,
+        language,
+        name,
+        ownerAvatarUrl,
+        ownerName,
+        ratingAverage,
+        reviewCount,
+        stargazersCount,
+      }
     }
   }
+}
 `;
--- part10/rate-repository-app/src/hooks/useRepositories.js
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -1,26 +1,38 @@
 import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
 
-  const fetchRepositories = async () => {
-    setLoading(true);
+  const { data, error, loading } =useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network'
+  });
+
+  const repositories = data ? data.repositories : undefined;
+  return { repositories };
 
 
-    const response = await fetch('http://192.168.20.5:5000/api/repositories');
+  // const [repositories, setRepositories] = useState();
+  // const [loading, setLoading] = useState(false);
 
-    const json = await response.json();
+  // const fetchRepositories = async () => {
+  //   setLoading(true);
 
-    setLoading(false);
-    setRepositories(json);
-  };
 
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
+  //   const response = await fetch('http://192.168.20.5:5000/api/repositories');
 
-  return { repositories, loading, refetch: fetchRepositories };
+  //   const json = await response.json();
+
+  //   setLoading(false);
+  //   setRepositories(json);
+  // };
+
+  // useEffect(() => {
+  //   fetchRepositories();
+  // }, []);
+
+  // return { repositories, loading, refetch: fetchRepositories };
 };
 
 export default useRepositories;
